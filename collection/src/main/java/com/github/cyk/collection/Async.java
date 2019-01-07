65d0da043b082c891fa991c767d59e53c3f379b9
cyk <chenyongke111@163.com>
2019-01-07 15:46:04 +0800

    countdownlatch 线程计数器使用 信号量 栅栏

--- collection/src/main/java/com/github/cyk/collection/Async.java
+++ collection/src/main/java/com/github/cyk/collection/Async.java
@@ -1,10 +1,13 @@
 package com.github.cyk.collection;
 
-import lombok.extern.slf4j.Slf4j;
+import com.google.common.util.concurrent.*;
+import io.netty.util.concurrent.DefaultEventExecutorGroup;
+import io.netty.util.concurrent.EventExecutorGroup;
+import io.netty.util.concurrent.FutureListener;
 import org.junit.Test;
 
-import java.util.Random;
-import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.*;
+import java.util.function.Supplier;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
@@ -43,20 +46,136 @@ public class Async {
     public void runAsyncExample()throws Exception {
         CompletableFuture<Void>cf = CompletableFuture.runAsync(() -> {
             assertTrue(Thread.currentThread().isDaemon());
-           /* try {
-                //Thread.sleep( Long.parseLong(Integer.parseInt(String.valueOf(Math.random()* 1000)).));
+            try {
+                Thread.sleep(10000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }*/
+            }
         });
         assertFalse(cf.isDone());
-        /*try {
-            Thread.sleep( Long.parseLong(String.valueOf(Math.random()* 1000 * 1000)));
+
+        try {
+            Thread.sleep(10000);
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }*/
-        assertTrue(cf.isDone());
+        }
+        //assertTrue(cf.isDone());
     }
+
+    @Test
+    public void beforeJdk8() throws Throwable, ExecutionException {
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        // Future代表了线程执行完以后的结果，可以通过future获得执行的结果
+        // 但是jdk1.8之前的Future有点鸡肋，并不能实现真正的异步，需要阻塞的获取结果，或者不断的轮询
+        // 通常我们希望当线程执行完一些耗时的任务后，能够自动的通知我们结果，很遗憾这在原生jdk1.8之前
+        // 是不支持的，但是我们可以通过第三方的库实现真正的异步回调
+        Future<String> f = executor.submit(new Callable<String>() {
+
+            @Override
+            public String call() throws Exception {
+                System.out.println("task started!");
+                //Thread.sleep(3000);
+                System.out.println("task finished!");
+                return "hello";
+            }
+        });
+
+        //此处阻塞main线程
+        System.out.println("==="+f.get());
+        System.out.println("main thread is blocked");
+    }
+
+
+    public void guavaFuture(){
+        ExecutorService executor = Executors.newFixedThreadPool(1);
+        // 使用guava提供的MoreExecutors工具类包装原始的线程池
+        ListeningExecutorService listeningExecutor = MoreExecutors.listeningDecorator(executor);
+        //向线程池中提交一个任务后，将会返回一个可监听的Future，该Future由Guava框架提供
+        ListenableFuture<String> lf = listeningExecutor.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println("task started!");
+                //模拟耗时操作
+                Thread.sleep(3000);
+                System.out.println("task finished!");
+                return "hello";
+            }
+        });
+        //添加回调，回调由executor中的线程触发，但也可以指定一个新的线程
+        Futures.addCallback(lf, new FutureCallback<String>() {
+            //耗时任务执行失败后回调该方法
+            @Override
+            public void onFailure(Throwable t) {
+                System.out.println("failure");
+            }
+            //耗时任务执行成功后回调该方法
+            @Override
+            public void onSuccess(String s) {
+                System.out.println("success " + s);
+            }
+        });
+        //主线程可以继续做其他的工作
+        System.out.println("main thread is running");
+    }
+
+
+    public void nettyFuture(){
+        //线程池
+        EventExecutorGroup group = new DefaultEventExecutorGroup(1);
+        //向线程池中提交任务，并返回Future，该Future是netty自己实现的future
+        //位于io.netty.util.concurrent包下，此处运行时的类型为PromiseTask
+        Future<?> ff = group.submit(new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println("任务正在执行");
+                //模拟耗时操作，比如IO操作
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("任务执行完毕");
+            }
+        });
+
+        //增加监听
+        ((io.netty.util.concurrent.Future<?>) ff).addListener(new FutureListener() {
+            @Override
+            public void operationComplete(io.netty.util.concurrent.Future future) throws Exception {
+                System.out.println("ok!!!");
+            }
+        });
+        System.out.println("main thread is running.");
+    }
+
+
+    public void jdk8Future(){
+        //两个线程的线程池
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+        //jdk1.8之前的实现方式
+        CompletableFuture<String> future = CompletableFuture.supplyAsync(new Supplier<String>() {
+            @Override
+            public String get() {
+                System.out.println("task started!");
+                try {
+                    //模拟耗时操作
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                return "task finished!";
+            }
+        }, executor);
+
+        //采用lambada的实现方式
+        future.thenAccept(e -> System.out.println(e + " ok"));
+
+        System.out.println("main thread is running");
+
+    }
+
+
 
 
 }
---
+++ collection/src/main/java/com/github/cyk/thread/CountDownLatchExample.java
@@ -0,0 +1,153 @@
+package com.github.cyk.thread;
+
+import org.junit.Test;
+
+import java.io.Writer;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
+public class CountDownLatchExample {
+
+    /**
+     * countdownlatch 主要是两个方法 countDown() 和await();  countDown() 用于计数器减1 ，一般执行任务的线程调用，await() 使线程处于等待状态，一把主线程调用
+     * countDown()方法并没有规定一个线程只能调用一次，当同一个线程调用多次countDown()方法时，每次都会使计数器减一；另外，await()方法也并没有规定只能有一个线程执行该方法，如果多个线程同时执行await()方法，那么这几个线程都将处于等待状态，并且以共享模式享有同一个锁
+     *
+     * CountDownLatch和CyclicBarrier都能够实现线程之间的等待，只不过它们侧重点不同：
+     *
+     * CountDownLatch一般用于某个线程A等待若干个其他线程执行完任务之后，它才执行；
+     *
+     * 而CyclicBarrier一般用于一组线程互相等待至某个状态，然后这一组线程再同时执行；
+     *
+     * 另外，CountDownLatch是不能够重用的，而CyclicBarrier是可以重用的。
+     *
+     * 2）Semaphore其实和锁有点类似，它一般用于控制对某组资源的访问权限。
+     *
+     */
+
+    final CountDownLatch latch = new CountDownLatch(2);
+
+    @Test
+    public void testCountDownLatch(){
+
+        new Thread(){
+            @Override
+            public void run(){
+                try {
+                    System.out.println("子线程1"+Thread.currentThread().getName()+"正在执行");
+                    Thread.sleep(3000);
+                    System.out.println("子线程1"+Thread.currentThread().getName()+"执行完毕");
+                    latch.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+
+
+        new Thread(){
+            @Override
+            public void run(){
+                try {
+                    System.out.println("子线程2"+Thread.currentThread().getName()+"正在执行");
+                    Thread.sleep(3000);
+                    System.out.println("子线程2"+Thread.currentThread().getName()+"执行完毕");
+                    latch.countDown();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+
+
+        try {
+            System.out.println("等待2个子线程执行完毕..."+Thread.currentThread().getName());
+            latch.await();
+            System.out.println("2个子线程已经执行完毕"+Thread.currentThread().getName());
+            System.out.println("继续执行主线程:"+Thread.currentThread().getName());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *  cyclicbarrier 可以重复 而CountDownLatch无法进行重复使用
+     * @throws Exception
+     */
+
+    @Test
+    public void testCyclicBarrier()throws Exception{
+        int N = 4;
+        //CyclicBarrier barrier  = new CyclicBarrier(4);
+
+        // 所有线程执行完成后，操作,任意选择一个去执行
+        CyclicBarrier barrier  = new CyclicBarrier(N,new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("当前线程"+Thread.currentThread().getName());
+            }
+        });
+        for(int i=0;i<N;i++){
+            new Writer(barrier).start();
+        }
+    }
+
+    static class Writer extends Thread{
+        private CyclicBarrier cyclicBarrier;
+        public Writer(CyclicBarrier cyclicBarrier) {
+            this.cyclicBarrier = cyclicBarrier;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("线程"+Thread.currentThread().getName()+"正在写入数据...");
+            try {
+                //以睡眠来模拟写入数据操作
+                //Thread.sleep(1000);
+                System.out.println("线程"+Thread.currentThread().getName()+"写入数据完毕，等待其他线程写入完毕");
+                cyclicBarrier.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }catch(BrokenBarrierException e){
+                e.printStackTrace();
+            }
+            System.out.println("所有线程写入完毕，继续处理其他任务...");
+        }
+    }
+
+    @Test
+    public void testSemaphore(){
+        int N = 8;            //工人数
+        Semaphore semaphore = new Semaphore(5); //机器数目
+        for(int i=0;i<N;i++) {
+            new Worker(i, semaphore).start();
+        }
+    }
+
+    static class Worker extends Thread{
+        private int num;
+        private Semaphore semaphore;
+        public Worker(int num,Semaphore semaphore){
+            this.num = num;
+            this.semaphore = semaphore;
+        }
+
+        @Override
+        public void run() {
+            try {
+                semaphore.acquire();
+                System.out.println("工人"+this.num+"占用一个机器在生产...");
+                //Thread.sleep(2000);
+                System.out.println("工人"+this.num+"释放出机器");
+                semaphore.release();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+}
